c0eede5441b4f37e197ee4dcb0c0b6e0b2b6f53a
cazalia <carolineazalia.xyz@gmail.com>
2025-06-18 18:38:07 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@ require_once 'data.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rental Mobil Online - Yudhiawan Dwi Nugraha</title>
+    <title>Rental Mobil Online</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
